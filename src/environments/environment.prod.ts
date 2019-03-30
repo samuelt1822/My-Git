64e3553a7fff0e83137e46ad0939f82b5ed6c25e
samuelt1822 <samuel.te@husky.neu.edu>
2019-03-30 15:40:35 -0500

    Push for Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   /** below added for a4 testing */
-  baseUrl: 'https://dashboard.heroku.com/apps/first-appcs5610'
+  baseUrl: 'https://first-appcs5610.herokuapp.com'
 };
